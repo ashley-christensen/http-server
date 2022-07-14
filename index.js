0e0810a5f4217e6ec9f39c700d0c21e23c8f20ba
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-07-14 13:18:41 -0700

    endpoints

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const http = require('http');
 
-const PORT = 3008;
+const PORT = 3009;
 
 const server = http.createServer((req, res) => {
- //write response header
- res.writeHead(200, {
-  'Content-Type': 'application/json',
- });
- //Data is ready, res.end expects a string
- res.end(JSON.stringify({
-  id: 1,
-  name: 'Sir Isaac Newton',
- }));
+ if (req.url === '/friends') {
+  // res.writeHead(200, {
+  //  'Content-Type': 'application/json',
+  // });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({//Data is ready, res.end expects a string
+   id: 1,
+   name: 'Sir Isaac Newton',
+  }));
+ } else if (req.url === '/messages') {
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<body>');
+  res.write('<ul>');
+  res.write('<li>Hello Ashley</li>');
+  res.write('<li>What are your thoughts on astronomy?</li>');
+  res.write('</ul>');
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
+ }
 });
 
 server.listen(PORT, () => {
