b640dbcf99bce02fc00d88a83c20ec341321c7d9
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-07-14 13:56:08 -0700

    split URL endpoints and index

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const http = require('http');
 
 const PORT = 3009;
+const friends = [
+ {
+  id: 0,
+  name: 'Nikola Tesla',
+ },
+ {
+  id: 1,
+  name: 'Sir Isaac Newton',
+ },
+ {
+  id: 2,
+  name: 'Albert Einstein',
+ },
+];
 
 const server = http.createServer((req, res) => {
- if (req.url === '/friends') {
-  // res.writeHead(200, {
-  //  'Content-Type': 'application/json',
-  // });
+ const items = req.url.split('/');
+ // /friends/2 => ['', 'friends', '2'];
+ if (items[1] === 'friends') {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({//Data is ready, res.end expects a string
-   id: 1,
-   name: 'Sir Isaac Newton',
-  }));
- } else if (req.url === '/messages') {
+  res.setHeader('Content-Type', 'application/json');
+  if (items.length === 3) { // user has specified 3rd item, parameter for unique item in colletion
+   const friendsIndex = Number(items[2]);
+   res.end(JSON.stringify(friends[friendsIndex]));
+  } else {
+   res.end(JSON.stringify(friends));
+  }
+
+ } else if (items[1] === 'messages') {
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<body>');
@@ -23,6 +39,9 @@ const server = http.createServer((req, res) => {
   res.write('</ul>');
   res.write('</body>');
   res.write('</html>');
+  res.end();
+ } else {
+  res.statusCode = 404;
   res.end();
  }
 });
